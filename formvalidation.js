8597c692d419e78189d6481ffe12d195beb44176
Noel Bryant <noelsobryant@gmail.com>
2023-07-28 14:43:43 +0300

    remove local storage functionality

--- formvalidation.js
+++ formvalidation.js
@@ -26,29 +26,4 @@ const loadFormDataFromLocalStorage = () => {
   return storedFormData ? JSON.parse(storedFormData) : {};
 };
 
-// Function to populate the form with the loaded data
-const populateFormWithStoredData = (formData) => {
-  document.getElementById('name').value = formData.name || '';
-  document.getElementById('email').value = formData.email || '';
-  document.getElementById('message').value = formData.message || '';
-};
 
-// Event listener for form submission
-document.getElementById('contactForm').addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  // Gather form data
-  const formData = {
-    name: document.getElementById('name').value,
-    email: document.getElementById('email').value,
-    message: document.getElementById('message').value,
-  };
-
-  // Save form data to local storage
-  saveFormDataToLocalStorage(formData);
-  document.getElementById('successmessage').innerHTML = 'Message sent';
-});
-
-// On page load, load form data from local storage and populate the form
-const storedFormData = loadFormDataFromLocalStorage();
-populateFormWithStoredData(storedFormData);
