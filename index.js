70d4e82f729412e2fc83378650514dad1417341d
Noel Bryant <noelsobryant@gmail.com>
2024-03-18 15:53:44 +0300

    style project works cards and add projects

--- index.js
+++ index.js
@@ -28,60 +28,67 @@ const projectsData = [
   {
     id: '1',
     name: 'To do list',
-    image: 'todolist.png',
+    image: 'todolist.jpg',
     company: 'solo project',
     role: 'fullstack',
     year: '2023',
     descrShort: 'a simple to do list application',
     descrLong:
       'Users can plan their schedules easily with this application. They can add to do items, edit existing to do items , sort the items and remove them once accomplished',
+    language: ['React', 'Redux'],
     details:
       'A daily selection of privately personalized reads; accounts or sign-ups required.',
     liveLink: 'https://noellincoln.github.io/to-do-list',
     sourceLink: 'https://github.com/NoelLincoln/to-do-list',
   },
   {
-    id: '3',
-    name: 'Tonic3',
-    image: 'snapshot2.png',
+    id: '2',
+    name: 'Air Pollution Stats',
+    image: 'air-pollution-stats.png',
     company: 'solo project',
     role: 'fullstack',
     year: '2023',
     descrShort:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+      'Air Pollution Stats is a webapp built with the aim of sharing information about air pollution for various regions around the globe.',
     descrLong:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    details:
-      "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-    liveLink: 'https://noellincoln.github.io',
-    sourceLink: 'https://github.com/NoelLincoln/noellincoln.github.io',
+      'This application provides statistics about air pollution. It is built with react ,redux and openweather api. When the application first loads, it geolocates the users device and loads the states from/in  the country. A user will then proceed to select a state and get data about the air quality index.',
+    language: ['React', 'Redux', 'REST API'],
+    details: 'Get air quality index data from all over the globe.',
+    liveLink: 'https://air-pollution-stats.onrender.com/',
+    sourceLink: 'https://github.com/NoelLincoln/air-pollution-stats',
   },
   {
     id: '3',
-    name: 'Multipost',
-    image: 'multipoststories.png',
+    name: 'AmannaTrust',
+    image: 'amannatrust.png',
     company: 'solo project',
     role: 'fullstack',
     year: '2023',
     descrShort:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      'Amannatrust is a project based on a non governmental organization formed with the aim to help needy children get access to education. The application is built with HTML, vanilla CSS and vanilla Javascript.',
     descrLong:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+      'The organization operates on a project-based model, strategically implementing initiatives geared towards providing access to education for those who need it most. At its core, Amannatrust is driven by a profound commitment to addressing the barriers that hinder children from receiving quality education. These barriers may include financial constraints, lack of educational infrastructure, societal challenges, or other forms of disadvantage. Through its projects, Amannatrust endeavors to dismantle these barriers, empowering children with the knowledge and skills they need to thrive.',
+    language: ['HTML', 'CSS', 'JavaScript'],
+
     details:
       "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-    liveLink: 'https://noellincoln.github.io',
-    sourceLink: 'https://github.com/NoelLincoln/noellincoln.github.io',
+    liveLink: 'https://noellincoln.github.io/first-capstone-project/',
+    sourceLink: 'https://github.com/NoelLincoln/first-capstone-project',
   },
 ];
 
 function popUpWindow(project) {
+  const languageListItems = project.language
+    .map((language) => `<li class='html-lan'>${language}</li>`)
+    .join('');
+
   const projectcontent = `
   <div class="popup-container  ">
      <div class="works-popup works-popup-desktop">
                     <div class="primary-text">
                         <div class="popup-close">
                             <h2>${project.name}</h2>
-                            <img src="assets/images/icon-cancel.png" id="closepopup">
+                            <img src="assets/images/close-icon.png" id="closepopup">
                         </div>
 
                         <div class="work-info work-info-popup-desktop">
@@ -111,10 +118,7 @@ function popUpWindow(project) {
                         </div>
                         <div class="right right-desktop">
                           <ul class="language-tags">
-                            <li class="html-lan">HTML</li>
-                            <li class="css">CSS</li>
-                            <li class="javascript">Javascript</li>
-
+                            ${languageListItems} 
                         </ul>
 
                         <div class="divider">
@@ -131,7 +135,7 @@ function popUpWindow(project) {
                            <a href="${project.sourceLink}" rel="noreferer">
                            <div class="view-project-btn " id="viewsourcebtn">
                                <p>See source</p>
-                               <a href="${project.sourceLink}" rel="noreferer">
+                               <a href="${project.sourceLink}" target="_blank" rel="noreferer">
                                <img src="assets/images/source-code.png" />   
                            </div>
                              </a>
@@ -180,9 +184,7 @@ function projectWorkCards(projectsData = []) {
                             <p> ${project.descrShort}.</p>
                         </div>
                         <ul class="language-tags">
-                            <li class="html-lan">HTML</li>
-                            <li class="css">CSS</li>
-                            <li class="javascript">Javascript</li>
+                            
                         </ul>
                         <div class="action">
                             <button class="action-button"  project-id="${project.id}" id="view-project">See project</button>
@@ -207,6 +209,8 @@ function openWindow(project) {
   projectContainer.style.display = 'block';
 
   const popUpCloseButton = document.querySelector('#closepopup');
+  popUpCloseButton.style.width = '50px';
+  popUpCloseButton.style.height = '28px';
 
   popUpCloseButton.addEventListener('click', closeWindow);
 }
